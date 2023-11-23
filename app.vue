e19f007221aa48d0a45fb6fd5695d3bd806994ac
Adeniyi Funmbi Adeyokunnu <funmbi.adeyokunnu@gmail.com>
2023-11-23 12:11:34 +0100

    ch: init base Nuxt app

---
+++ app.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <Head>
+      <Title>Kisi Mosaic</Title>
+    </Head>
+
+    <h3>Index</h3>
+  </div>
+</template>
+
+<script setup lang="ts">
+useSeoMeta({
+  title: '[title]',
+  // description: '[description]',
+  // ogTitle: '[og:title]',
+  // ogDescription: '[og:description]',
+  // ogImage: '[og:image]',
+  // ogUrl: '[og:url]',
+  // twitterTitle: '[twitter:title]',
+  // twitterDescription: '[twitter:description]',
+  // twitterImage: '[twitter:image]',
+  // twitterCard: 'summary'
+})
+
+useHead({
+  htmlAttrs: {
+    lang: 'en'
+  },
+  link: [
+    // {
+    //   rel: 'icon',
+    //   type: 'image/png',
+    //   href: '/favicon.png'
+    // }
+  ]
+})
+</script>
