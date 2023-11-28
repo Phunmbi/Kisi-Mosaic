8517282d233eec1471990257da61a04acec143a2
Adeniyi Funmbi Adeyokunnu <funmbi.adeyokunnu@gmail.com>
2023-11-28 13:32:18 +0100

    ch: restructure app for base deployment to render

--- backend/index.ts
+++ backend/index.ts
@@ -1,73 +1,76 @@
-import express from "express";
-import { createServer } from "http";
-import { readdir } from "node:fs/promises";
-import formidable from "formidable";
-import path from "node:path";
-import cors from "cors";
-import data from "./data/articles.json";
+import { createServer } from 'http'
+import { readdir } from 'node:fs/promises'
+import path from 'node:path'
+import formidable from 'formidable'
+import cors from 'cors'
+import 'dotenv/config'
+import express from 'express'
+import data from './data/articles.json'
 
-const app = express();
-const port = 4000 || process.env.PORT;
+const app = express()
+const port = process.env.PORT || 4000
 
-createServer(app);
+createServer(app)
+const webURL = process.env.WEB_URL
 
 app.use(
   cors({
-    origin: 'http://localhost:3000'
+    origin: webURL
   })
 )
-app.use('/assets', express.static(path.join(__dirname, 'assets')))
+app.use('/assets', express.static(path.join(__dirname, 'images')))
 
 app.listen(port, () => {
-  console.log(`Kisi API Server listening on ${port}`);
-});
+  console.log(`Kisi API Server listening on ${port}`)
+})
 
-app.get('/api/images', async (req, res) => {
+app.get('/api/images', async (_, res) => {
   try {
-    const files  = await readdir( path.join(__dirname, "assets") );
+    const files = await readdir(path.join(__dirname, 'images'))
     const articles: {
       title: string,
       description: string,
       img?: string
-    } [] = [... data];
-    
-    files.map( async (image, idx) => { articles[idx].img = `http://localhost:4000/assets/${image}` })
+    } [] = [...data]
 
+    files.map((image, idx) => {
+      articles[idx].img = `/assets/${image}`
+      return articles
+    })
 
     res.status(200).json({
       articles
     })
     return { articles }
   } catch (error) {
-    console.error(error?.toString())
+    res.status(400).json({
+      message: 'error fetching images'
+    })
   }
 })
 
-app.post('/api/images' , async (req, res) => {
-  const files  = await readdir( path.join(__dirname, "assets"));
-  const uploadFolder = path.join(__dirname, "assets");
-
-  const form = formidable({
-    uploadDir: uploadFolder,
-    maxFileSize: 5 * 1024 * 1024,
-    filename: (_, __, part) => {
-      const { mimetype }= part
-      let ext = (mimetype as string).split('/');
-      console.log(mimetype, ext[1])
-      return `image${files.length}.${ext[1]}`
-    }
-  });
+app.post('/api/images', async (req, res) => {
+  const uploadFolder = path.join(__dirname, 'images')
+  const files = await readdir(uploadFolder)
 
   try {
-   await form.parse(req)
+    const form = formidable({
+      uploadDir: uploadFolder,
+      maxFileSize: 5 * 1024 * 1024, // max image size upload is capped at 5MB
+      filename: (_, __, part) => {
+        const { mimetype } = part
+        const ext = (mimetype as string).split('/')
+        return `image${files.length}.${ext[1]}`
+      }
+    })
+    await form.parse(req)
 
-   res.status(200).json({
-    message: "upload successful"
-   })
-
+    res.status(200).json({
+      message: 'upload successful'
+    })
   } catch (error) {
     res.status(400).json({
-      message: "failed upload",
+      message: 'failed upload',
       error: error.toString()
     })
   }
